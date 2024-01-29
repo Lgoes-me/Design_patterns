9fc9d0d818091141fd44bf69ae1b02ef0dcbd894
Góes <lucas.rodrigues.goes@gmail.com>
2024-01-29 01:28:40 -0300

    BaseBuilder

---
+++ Assets/_Game/Builder/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Builder.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [field: SerializeField] public string Name { get; internal set; }
+        [field: SerializeField] public int MaxHealth { get; internal set; }
+        [field: SerializeField] public int Damage { get; internal set; }
+        [field: SerializeField] public float Speed { get; internal set; }
+    }
+
+    public class EnemyBuilder
+    {
+        private EnemyController EnemyController { get; set; }
+
+        public EnemyBuilder(EnemyController enemyController)
+        {
+            EnemyController = enemyController;
+        }
+
+        public EnemyBuilder Name(string name)
+        {
+            EnemyController.Name = name;
+            return this;
+        }
+
+        public EnemyBuilder MaxHealth(int maxHealth)
+        {
+            EnemyController.MaxHealth = maxHealth;
+            return this;
+        }
+
+        public EnemyBuilder Damage(int damage)
+        {
+            EnemyController.Damage = damage;
+            return this;
+        }
+
+        public EnemyBuilder Speed(float speed)
+        {
+            EnemyController.Speed = speed;
+            return this;
+        }
+
+        public EnemyController Build()
+        {
+            return EnemyController;
+        }
+    }
+
+    public class EnemyDirector
+    {
+        public EnemyController BuildDefaultEnemy(EnemyController enemyController)
+        {
+            return new EnemyBuilder(enemyController)
+                .Name("DefaultEnemy")
+                .MaxHealth(1000)
+                .Damage(10)
+                .Speed(100)
+                .Build();
+        }
+    }
+}
---
+++ Assets/_Game/Builder/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using Builder.Controllers;
+using UnityEngine;
+
+namespace Builder.Managers
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        [field: SerializeField] private EnemyController EnemyControllerPrefab { get; set; }
+
+        public void CreateNewEnemyWithBuilder()
+        {
+            var defaultEnemy = Instantiate(EnemyControllerPrefab);
+            
+            new EnemyBuilder(defaultEnemy)
+                .Name("Enemy")
+                .MaxHealth(1000)
+                .Damage(10)
+                .Speed(100)
+                .Build();
+        }
+        
+        public void CreateDefaultEnemyDirector()
+        {
+            var defaultEnemy = Instantiate(EnemyControllerPrefab);
+            new EnemyDirector().BuildDefaultEnemy(defaultEnemy);
+        }
+    }
+}
